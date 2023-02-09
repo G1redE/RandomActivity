4ac98d274ae8cb6c7fdecd8211d2759046f305dd
G1redE <90474166+G1redE@users.noreply.github.com>
2023-02-09 21:26:18 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
+import React from 'react'
 import './App.css';
 
 function App() {
+  const [activity,setActivity] = React.useState('Bored? Click on the Button , I will suggest an activity')
+  const fetchData = async() => {
+    const res = await fetch('https://www.boredapi.com/api/activity');
+    const data = await res.json()
+    setActivity(data.activity)
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="innerDiv">
+      <h1>{activity}</h1>
+      <button className='searchButton' onClick={fetchData}>Click Me</button>
+      </div>
     </div>
   );
 }
